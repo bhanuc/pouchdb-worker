c63a2672f8c7bda8db73362f82894f75c85f4c39
Bhanu Pratap Chaudhary <bhanu423@gmail.com>
2016-01-05 10:37:06 +0000

    added vanillajs example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Source Code for article to use pouch.db using web-workers. The article will not delve into heavy use of web-workers but will
 make you aware of strategies of using pouchdb using VanillaJS, Browserify, Ember.js, WebPack, Worker-Pouch
+
+Do npm install and open index.html
---
+++ VanillaJS/pouch-worker.js
@@ -0,0 +1,48 @@
+self.importScripts('../node_modules/pouchdb/dist/pouchdb.min.js');
+
+var db = new PouchDB('workers');
+
+onmessage = function(dbEvent) {
+    var data = dbEvent.data;
+    switch (data.msg) {
+        case 'CREATE':
+            var id = Date.now().toString();
+            var todo = data.todo;
+            db.put({
+                _id: id,
+                todo: todo
+            }).then(function(response) {
+                // handle response
+                postMessage({
+                    'msg': 'CREATE',
+                    response: response,
+                    todo: todo
+                });
+            }).catch(function(err) {
+                postMessage({
+                    'msg': 'ERROR',
+                    response: err
+                });
+            });
+            break;
+        case 'DELETE':
+            var id = data.id;
+            db.get(id).then(function(doc) {
+                return db.remove(doc._id, doc._rev);
+            }).then(function(result) {
+                postMessage({
+                    'msg': 'DELETE',
+                    id: id
+                });
+            }).catch(function(err) {
+                // console.log(err);
+                postMessage({
+                    'msg': 'ERROR',
+                    response: err
+                });
+            });
+            break;
+        default:
+            // code
+    }
+};
